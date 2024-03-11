5799aa898c05477b314f5567bc31ed80e4791557
JacopoLombardi <lombajacopo@gmail.com>
2024-03-11 17:53:09 +0100

    [add] added last esercise

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ vue-hello
 1. Inizio a costruirmi la struttura di base di Vue su Js, e sull'HTML.
 2. Stampare sul DOM un messaggio tramite il metodo 'data' di Vue.
 3. Creare alcuni piccoli progetti per prendere la mano su Vue.
-4. 
--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 const {createApp} = Vue;
 
 
+
 // dentro il metodo createApp scriviamo i nostri codici 
 createApp({
 
@@ -18,12 +19,17 @@ createApp({
 
          indiceWithoutCounter: 0,
 
-         isBold: false,
+         indiceMultiple: 0,
+         multiple3: false,
+         multiple4: false,
+         multiple3and4: false,
+
+         isBold: false
       };
    },
 
 
-   
+
 
    // è un oggetto del metodo 'createApp'
    methods:{
@@ -35,8 +41,22 @@ createApp({
       contatore(){
          this.indice++;
          console.log('contatore ---- ',this.indice);     // stampo un contatore con una funzione quando clicco il btn
-      }
+      },
 
+      isMultiple3or4(){
+         this.multiple3 = false;
+         this.multiple4 = false;
+         this.multiple3and4 = false;
+         this.indiceMultiple++;
+
+         if(this.indiceMultiple % 3 === 0  &&  this.indiceMultiple % 4 === 0){     // confronto se il numero del contatore è multiplo di 3, 4 o di entrambi
+            this.multiple3and4 = true;
+         }else if(this.indiceMultiple % 3 === 0){
+            this.multiple3 = true;
+         }else if(this.indiceMultiple % 4 === 0){
+            this.multiple4 = true;
+         }
+      }
    }
 
 }).mount('#app');
